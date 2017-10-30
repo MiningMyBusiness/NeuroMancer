ed21cf51b3882fb8f8c29a0e934bc7e2fb4175b2
Kiran Bhattacharyya <bhattacharyyakiran12@gmail.com>
2017-10-29 19:15:03 -0500

    Delete Master.py

--- Code/Master.py
+++
@@ -1,9 +0,0 @@
-# Title: Master Abstract Analyzer
-# Description: Master run file to abstract analysis of neuroscience review articles
-#   will run all relevant code files and generate output.
-# Author: Kiran Bhattacharyya
-# License: MIT License
-
-execfile('TextExtractor.py') # run word extractor
-execfile('AbstractSummarizer.py') # run keyword Extractor
-execfile('KeywordAnalyzer_FINAL.py') # run keyword analyzer
